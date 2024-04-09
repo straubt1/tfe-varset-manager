5fcfd61ee2743f46477a991840ec798d0de024a9
straubt1 <tstraub@hashicorp.com>
2024-04-09 15:16:08 -0500

    exampel

--- example/main.tf
+++ example/main.tf
@@ -2,21 +2,60 @@ module "example" {
   source = "../"
 
   organization_name = "terraform-tom"
-  variable_sets = {
-    "varset-1" = {
-      projects = ["Project 1", "Project-AIT"]
-      // name        = "varset-1"
+  variable_sets = [
+    {
+      name        = "varset-1"
       description = "This is a TF created varset."
-      variables = {
-        vault_cluster_address = {
-          // key = "vault_cluster_address"
+      projects    = ["Project 1"]
+      variables = [
+        {
+          key         = "vault1_cluster_address"
+          value       = "https://vault1.company.com"
+          description = "Vault1 FQDN"
+          sensitive   = false
+          category    = "terraform"
+          hcl         = false
+        },
+        {
+          key         = "vault2_cluster_address"
+          value       = "https://vault2.company.com"
+          description = "Vault2 FQDN"
+          sensitive   = false
+          category    = "terraform"
+          hcl         = false
+        },
+        {
+          key         = "vault3_cluster_address"
+          value       = "https://vault3.company.com"
+          description = "Vault3 FQDN"
+          sensitive   = false
+          category    = "terraform"
+          hcl         = false
+        }
+      ]
+    },
+    {
+      name        = "varset-2"
+      description = "This is a TF created varset."
+      projects    = ["Project 1", "Project-AIT"]
+      variables = [
+        {
+          key         = "vault_cluster_address"
           value       = "https://vault.company.com"
           description = "Vault FQDN"
           sensitive   = false
           category    = "terraform"
           hcl         = false
+        },
+        {
+          key         = "vault3_cluster_address"
+          value       = "https://vault3.company.com"
+          description = "Vault3 FQDN"
+          sensitive   = false
+          category    = "terraform"
+          hcl         = false
         }
-      }
+      ]
     }
-  }
+  ]
 }
